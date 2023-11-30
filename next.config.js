82b1ee51bbf8bceaf1cf190c07c4aac61c44dd79
Nadzeya Kuzmitch <nk83@njit.edu>
2023-11-29 23:14:06 -0500

    update baspath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: 'https://nadzeyakuzmitch.github.io/to-do-manager/',
+    basePath: '/to-do-manager',
 }
 
 module.exports = nextConfig
